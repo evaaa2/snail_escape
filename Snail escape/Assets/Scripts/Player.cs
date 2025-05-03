1684141581eab34e488589f16e8fe564af1b16c1
Eva Dekastello <eva.dekastello@email.cz>
2025-05-03 20:31:18 +0200

    a bit moving

--- Snail escape/Assets/Scripts/Player.cs
+++ Snail escape/Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+        //the snail should not jump
+        moveY = 0;
 
         transform.position += new Vector3(moveX, moveY, 0f) * speed * Time.deltaTime;
     }
